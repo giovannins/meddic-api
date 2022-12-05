32871f976ad81b1236f9a1673604d71f8f2cc309
giovanni neves <giovanni_neves@tutanota.com>
2022-12-05 19:17:55 -0300

    Puxar todos usuários

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use ErrorException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -10,8 +11,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UsersController extends AbstractController
 {
-    #[Route('/users', name: 'app_users')]
-    public function index(Request $request): JsonResponse
+    #[Route('/users', name: 'users_all', methods: ['GET'])]
+    public function index(Request $request, UserRepository $userRepository): JsonResponse
     {
 
         $body = $request->toArray();
@@ -20,8 +21,19 @@ class UsersController extends AbstractController
             throw new ErrorException("Your key is not valid");
         }
 
-        return $this->json([
-            'a' => 'b'
-        ]);
+        return $this->json($userRepository->findAll());
+    }
+
+    #[Route('/users/{id}', name: 'users_id', methods: ['GET'])]
+    public function getId(Request $request, UserRepository $userRepository, int $id): JsonResponse
+    {
+
+        $body = $request->toArray();
+
+        if (!password_verify($body['nkey'], $body['ckey'])) {
+            throw new ErrorException("Your key is not valid");
+        }
+
+        return $this->json($userRepository->find($id));
     }
 }
