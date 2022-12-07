df54b356f0cadeea1c18a42f2951d6ddb7ea8c88
giovanni neves <giovanni_neves@tutanota.com>
2022-12-07 00:03:15 -0300

    Teste user controller

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use ErrorException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -35,5 +37,39 @@ class UsersController extends AbstractController
         }
 
         return $this->json($userRepository->find($id));
+    }
+
+    #[Route('/users/name/{name}', name: 'users_name', methods: ['GET'])]
+    public function getUserName(Request $request, UserRepository $userRepository, string $name): JsonResponse
+    {
+
+        $body = $request->toArray();
+
+        if (!password_verify($body['nkey'], $body['ckey'])) {
+            throw new ErrorException("Your key is not valid");
+        }
+
+        return $this->json($userRepository->findOneBy(['name' => $name]));
+    }
+
+    public function addUser(Request $request, ManagerRegistry $managerRegistry): JsonResponse
+    {
+        $body = $request->toArray();
+
+        if (!password_verify($body['nkey'], $body['ckey'])) {
+            throw new ErrorException("Your key is not valid");
+        }
+        
+        $manager = $managerRegistry->getManager();
+
+        $user = new User();
+        $user->setName($body['name']);
+        $user->setEmail($body['email']);
+        $user->setKey('key');
+        $user->setPassword(password_hash($body['password'], PASSWORD_BCRYPT));
+        $user->setManager(false);
+        
+
+        return $this->json($user->getId());
     }
 }
